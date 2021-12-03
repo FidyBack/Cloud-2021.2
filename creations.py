87ce237abe677bc4946a323d3f48f4968c3a8a24
FidyBack <abel.cavalcante.ismart@gmail.com>
2021-12-03 15:36:49 -0300

    Quase final

---
+++ creations.py
@@ -0,0 +1,135 @@
+import time
+
+
+def instance_creation(resource, imgId, UserData, key, instance_name, sgList = []):
+    new_instance = resource.create_instances(
+        ImageId=imgId,
+        UserData=UserData,
+        SecurityGroupIds=sgList,
+        InstanceType='t2.micro',
+        KeyName=key,
+        MinCount=1, MaxCount=1,
+        TagSpecifications=[{
+            'ResourceType': 'instance',
+            'Tags': [{
+                'Key': 'Name',
+                'Value': instance_name
+            }]
+        }]
+    )
+
+    instance = resource.Instance(new_instance[0].id)
+    instance.wait_until_exists()
+
+    while instance.public_dns_name == '':
+        try:
+            instance.load()
+        except:
+            time.sleep(0.5)
+
+    print(f"Instância em Ohio criada:\n\tID: {instance.id}\n\tDNS Público: {instance.public_dns_name}\n")
+
+    return instance
+
+
+def image_creation(instance, resource, img_name):
+    new_image = instance.create_image(Name=img_name)
+    image = resource.Image(new_image.image_id)
+
+    print("Criando Imagem...")
+    while image.state== 'pending':
+        time.sleep(2)
+        image.load()
+
+    instance.terminate()
+
+    print(f"\tImagem '{image.image_id}' foi criada da Instância '{instance.instance_id}'\n\tA Instância '{instance.instance_id}' foi terminada ")
+
+    return image
+
+
+def load_balancer_creation(client, lb_name, tg_name, sgList = []):
+    load_balancer = client.create_load_balancer(
+        Name=lb_name,
+        Subnets=[
+            'subnet-759aae3f',
+            'subnet-125f3e4e',
+            'subnet-d787e3b0',
+            'subnet-62afcc4c',
+            'subnet-1957ff27',
+            'subnet-4e8ad441',
+        ],
+        SecurityGroups=sgList,
+        Type='application'
+    )
+    lb_ARN = load_balancer['LoadBalancers'][0]['LoadBalancerArn']
+
+    target_groups = client.create_target_group(
+        Name=tg_name,
+        Protocol='HTTP',
+        Port=8080,
+        VpcId='vpc-3cca7a46',
+        HealthCheckProtocol='HTTP',
+        HealthCheckPath='/admin/login/?next=/admin'
+    )
+    tg_ARN = target_groups['TargetGroups'][0]['TargetGroupArn']
+
+    listener = client.create_listener(
+        LoadBalancerArn=lb_ARN,
+        Protocol='HTTP',
+        Port=80,
+        DefaultActions=[
+            {
+                'Type': 'forward',
+                'TargetGroupArn': tg_ARN,
+            }
+        ]
+    )
+    ln_ARN = listener['Listeners'][0]['ListenerArn']
+
+    lb_waiter = client.get_waiter('load_balancer_available')
+    lb_waiter.wait(LoadBalancerArns=[lb_ARN])
+
+    print(f'\tLoad Balancer: {lb_ARN}\n\tTarget Group: {tg_ARN}\n\tListener: {ln_ARN}')
+
+    return lb_ARN, tg_ARN, ln_ARN
+
+
+def autoscaling_creation(client, as_client, lt_name, as_name, image_id, key, tg_arm, sgList = []):
+    scaling_template = client.create_launch_template(
+        LaunchTemplateName=lt_name,
+        LaunchTemplateData={
+            'ImageId': image_id,
+            'InstanceType': 't2.micro',
+            'KeyName': key,
+            'SecurityGroupIds': sgList
+        }
+    )
+
+    template_id = scaling_template['LaunchTemplate']['LaunchTemplateId']
+
+    auto_scaling = as_client.create_auto_scaling_group(
+        AutoScalingGroupName=as_name,
+        LaunchTemplate={
+            'LaunchTemplateId': template_id
+        },
+        AvailabilityZones=[
+            'us-east-1a',
+            'us-east-1b',
+            'us-east-1c',
+            'us-east-1d',
+            'us-east-1e',
+            'us-east-1f',
+        ],
+        TargetGroupARNs=[
+            tg_arm
+        ],
+        MinSize=1,
+        MaxSize=100
+    )
+
+    auto_scaling_desc = as_client.describe_auto_scaling_groups(AutoScalingGroupNames=[as_name])
+
+    print(f"\tLaunch Template: {template_id}\n\tAuto Scaling: {auto_scaling_desc['AutoScalingGroups'][0]['AutoScalingGroupARN']}")
+
+    return auto_scaling
---
+++ deletions.py
@@ -0,0 +1,71 @@
+import time
+
+def instance_termination(resource, instance_name):
+    print(f"Encerrando Instâncias {instance_name}...")
+    resource.instances.filter(
+        Filters=[
+            {
+                'Name': 'tag:Name',
+                'Values': [
+                    f'{instance_name}',
+                ]
+            }
+        ]
+    ).terminate()
+
+
+def image_termination(resource, image_name):
+    print(f"Encerrando Imagens {image_name}...")
+    for image in resource.images.filter(
+        Filters=[
+            {'Name': 'name', 'Values': [f'{image_name}']}
+        ]
+    ):
+        if image.name == image_name:
+            image.deregister()
+
+
+def load_balancer_termination(client, lb_name):
+    paginator_lb = client.get_paginator('describe_load_balancers')
+    paginator_ls = client.get_paginator('describe_listeners')
+    paginator_tg = client.get_paginator('describe_target_groups')
+
+    try:
+        for lb in paginator_lb.paginate(Names=[lb_name]):
+            lb_ARN = lb['LoadBalancers'][0]['LoadBalancerArn']
+    except:
+        print(f"Load Balancer {lb_name} Inexistente")
+        return None
+
+    for ls in paginator_ls.paginate(LoadBalancerArn=lb_ARN):
+        ls_ARN = ls['Listeners'][0]['ListenerArn']
+
+    for tg in paginator_tg.paginate(LoadBalancerArn=lb_ARN):
+        tg_ARN = tg['TargetGroups'][0]['TargetGroupArn']
+
+    print(f"Encerrando Listener {ls_ARN}...")
+    client.delete_listener(ListenerArn=ls_ARN)
+    print(f"Encerrando Target Group {tg_ARN}...")
+    client.delete_target_group(TargetGroupArn=tg_ARN)
+    print(f"Encerrando Load Balancer {lb_ARN}...")
+    client.delete_load_balancer(LoadBalancerArn=lb_ARN)
+    
+
+def autoscaling_termination(client, as_client, lt_name, as_name):
+    try:
+        as_client.delete_auto_scaling_group(AutoScalingGroupName=as_name, ForceDelete=True)
+        auto_scaling = as_client.describe_auto_scaling_groups(AutoScalingGroupNames=[as_name])
+        print(f"Encerrando Auto Scaling {as_name}...")
+
+        while auto_scaling['AutoScalingGroups'][0]['Status'] == 'Delete in progress':
+            auto_scaling = as_client.describe_auto_scaling_groups(AutoScalingGroupNames=[as_name])
+            time.sleep(10)
+    except:
+        print(f"Auto Scaling {as_name} Inexistente")
+
+
+    try:
+        client.delete_launch_template(LaunchTemplateName=lt_name)
+        print(f"Encerrando Lauch Template {lt_name}...")
+    except:
+        print(f"Lauch Template {lt_name} Inexistente")
